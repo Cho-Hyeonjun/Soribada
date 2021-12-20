826703a5f92a076b4e0ce39110647772ac5e3d38
Cho-Hyeonjun <95673836+Cho-Hyeonjun@users.noreply.github.com>
2021-12-20 15:55:20 +0900

    1220

--- js/script.js
+++ js/script.js
@@ -1,15 +1,30 @@
 $(document).ready(function(){ //START
 
-  var swiper = new Swiper(".mySwiper", {
+
+  var swiper2 = new Swiper(".mySwiper1", {
+    slidesPerView: 3,
+    spaceBetween: 40,
+    centeredSlides: true,
+    loop: true,
+    initialSlide:1,
+    pagination: {
+      el: ".mySwiper1 .swiper-pagination",
+      clickable: true,
+    },
+  });
+  
+  var swiper1 = new Swiper(".mySwiper2", {
     slidesPerView: 2.5,
     spaceBetween: 40,
     centeredSlides: true,
     loop: true,
     initialSlide:1,
     pagination: {
-      el: ".swiper-pagination",
+      el: ".mySwiper2 .swiper-pagination",
       clickable: true,
     },
   });
-  
+
+
+
 }); //END
